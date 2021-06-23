98eb78d34fc20af27972a14697329367bb71a671
wangruilin <bluesbonewong@gmail.com>
2021-06-23 16:15:08 +0800

    express学习环境搭建完毕

---
+++ app2.ts
@@ -0,0 +1,12 @@
+import express = require("express");
+
+const app = express();
+const port = 8888;
+
+app.get("/", (req, res) => {
+    res.send("好\n");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
